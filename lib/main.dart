8a66ebf33c5e18f0181edeab2ffe9abf17cdb293
SOUNISH NATH <flock.sinasini@gmail.com>
2019-03-17 10:18:17 +0530

    lines of code added Controller for animation

--- lib/main.dart
+++ lib/main.dart
@@ -31,13 +31,51 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
 // defining colors pallete
  var appColors = [Color.fromRGBO(231, 129, 109, 1.0),Color.fromRGBO(99, 138, 223, 1.0),Color.fromRGBO(111, 194, 173, 1.0)];
 // card INdex
+  var cardIndex = 0;
+  ScrollController scrollController;
+  var currentColor = Color.fromRGBO(231, 129, 109, 1.0);
+
+  var cardList = [
+    new CardItemModel("Personal", Icons.account_circle,9,0.83),
+    new CardItemModel("Working", Icons.work,12,0.24),
+    new CardItemModel("Home", Icons.home,7,0.35)
+    ];
+
+  // animations
+  AnimationController animationController;
+  ColorTween colorTween;
+  CurvedAnimation curvedAnimation;
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-         child: new Text("hello Sounish"),
-      ),
-    );
+  void initState() {
+    super.initState();
+    scrollController = new ScrollController();
   }
+
+    @override
+    Widget build(BuildContext context) {
+      return Center(
+        child: Container(
+           child: new Text("hello Sounish"),
+        ),
+      );
+    }
+  }
+
+
+
+// crate a cardItem class
+  class CardItemModel {
+  String CardTitle;
+  IconData icon;
+  int tasksremaining;
+  double taskCompletation;
+
+ // creating a construction function
+ CardItemModel(
+   this.CardTitle,
+   this.icon,
+   this.tasksremaining,
+   this.taskCompletation
+   );
 }
